e586a3c63fcc6eb6087437d23977d4fb8e9e58ff
dimasyoga42 <dimasyoga9090@gmail.com>
2026-01-22 10:24:14 +0700

    first commit

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -2,4 +2,4 @@ import { SupabaseClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const supabase = new SupabaseClient(process.env.DB_URL, process.env.DB_KEY);
+export const supabase = new SupabaseClient("https://dpzomyklvfwauezwmdja.supabase.co", process.env.DB_KEY);
